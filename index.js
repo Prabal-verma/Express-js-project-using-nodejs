bcdfd7cc05a0bd76d3f87c565c2b565d8d5344cf
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-03-25 22:25:45 +0530

    Express first project (user database)

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
+const app = express();
+
+const PORT = 8000;
+
+//Middleware
+
+app.use(express.urlencoded({extended: false}));
+
+// ROUTES
+
+app.get("/api/users", (req,res)=>{
+  return res.json(users);
+});
+
+app.get("/users",(req,res)=>{
+  const HTML = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  </ul>`;
+  res.send(HTML);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+  const id = Number(req.params.id);
+  const user = users.find((user)=>user.id === id);
+  return res.json(user);
+});
+
+app.post("/api/users",(req,res)=>{
+  const body = req.body;
+  users.push({...body, id : users.length+1});
+  fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err,data)=>{
+    return res.json({status : "success" , id : users.length});
+  });
+});
+
+app.listen(PORT,()=>{
+  console.log(`Server started at Port ${PORT}`);
+});
