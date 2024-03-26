443c82777a8c2fa3c9a10d041622a3d8ced0a294
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-03-26 22:59:39 +0530

    Organised code in their respective folder

--- index.js
+++ index.js
@@ -1,107 +1,29 @@
 const express = require("express");
-const fs = require("fs");
+const { connectMongoDb } = require('./connection');
+const userRouter = require("./routes/user");
+const{logReqRes} = require("./middlewares/index");
 const app = express();
-const mongoose = require("mongoose");
-
 const PORT = 8000;
 
 // Connection
 
-mongoose.connect("mongodb://127.0.0.1:27017/test")
-.then(()=> {
-  console.log("MongoDb connected");
+connectMongoDb("mongodb://127.0.0.1:27017/test")
+.then(()=>{
+  console.log("connected!!");
 })
 .catch(()=>{
-  console.log("Mongo Error", err);
-})
-
-//Schema
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type : String,
-    required : true,
-  },
-  lastName: {
-    type : String,
-  },
-  email:{
-    type: String,
-    required: true,
-    unique : true,
-  },
-  jobTitle:{
-    type: String,
-
-  },
-  gender:{
-    type: String,
-  },
-},{
-  timestamps: true
+  console.log("error",err)
 });
 
-const User = mongoose.model("user", userSchema);
-
+//Schema
 //Middleware-Plugin
 
 app.use(express.urlencoded({extended: false}));
-
-app.use((req,res,next)=>{
-  fs.appendFile("log.txt", `\n${Date.now()}: ${req.ip}: ${req.method}: ${req.path}`,(err,data)=>{
-    next();
-
-  });
-});
+app.use(logReqRes("log.txt"));
 
 // ROUTES
 
-app.get("/api/users", async(req,res)=>{
-  const allDbUsers = await User.find({});
-  return res.json(allDbUsers);
-});
-
-app.get("/users",async(req,res)=>{
-  const allDbUsers = await User.find({});
-  const HTML = `
-  <ul>
-  ${allDbUsers.map((user) => `<li>${user.firstName} - ${user.email}</li>`).join("")}
-  </ul>`;
-  res.send(HTML);
-});
-
-app
-.route("/api/users/:id")
-.get(async(req,res)=>{
-  const user = await User.findById(req.params.id);
-  if(!user) return res.status(404).json({error : "User Not Found"});
-  return res.json(user);
-})
-.patch(async(req,res)=>{
-  await User.findByIdAndUpdate(req.params.id , {lastName : "Changed"});
-  return res.json("Success");
-})
-.delete(async(req,res)=>{
-  await User.findByIdAndDelete(req.params.id);
-  return res.json({status : "Success"});
-})
-
-app.post("/api/users",async(req,res)=>{
-  const body = req.body;
-  if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
-    res.status(400).json({msg: "All fields are required..."})};
-
-    const result = await User.create({
-      firstName: body.first_name,
-      lastName: body.last_name,
-      email: body.email,
-      gender: body.gender,
-      jobTitle: body.job_title,
-    });
-
-    return res.status(201).json({msg: "success"});
-});
-
+app.use("/api/users",userRouter);
 app.listen(PORT,()=>{
   console.log(`Server started at Port ${PORT}`);
 });
